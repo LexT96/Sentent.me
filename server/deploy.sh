4f820a52b4884e62ef2f043a01e6393a41dac708
Thorsten <tlex1@web.de>
2021-10-10 22:22:07 +0200

    Update deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,5 @@
-pipenv lock
+ssh -i "pystonks.pem" ubuntu@ec2-18-194-112-217.eu-central-1.compute.amazonaws.com "cd pyStonks && git pull origin main && pipenv install --deploy --ignore-pipfile"
+
 # git add .
 # git commit -m "Deploying"
 # git push origin main
-ssh -i "pystonks.pem" ubuntu@ec2-18-194-112-217.eu-central-1.compute.amazonaws.com cd pyStonks && git pull origin main && pipenv install --deploy --ignore-pipfile

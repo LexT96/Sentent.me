92ac76b1054db3a681da10afd6fd0237af00990e
Thorsten <tlex1@web.de>
2021-10-10 16:27:51 +0200

    Add missing import

--- server/jobs/fetchCronjob.py
+++ server/jobs/fetchCronjob.py
@@ -2,6 +2,7 @@ from app.services.stock_api_service import fetch_tickers
 from app.services.db_service import insert_stocks, insert_entry
 from app.services.reddit_service import get_posts, group_posts_by_stock
 from app.services.sentiment_service import add_sentiment_to_posts, calculate_average_sentiment
+from datetime import datetime
 
 def fetchJob():
     print("OK")

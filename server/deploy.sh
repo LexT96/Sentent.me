790624a6a10a81f60262d274cfba6d5dfa2add6a
Thorsten <tlex1@web.de>
2021-10-12 22:48:00 +0200

    Fix deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,4 +1,4 @@
-ssh -i "pystonks.pem" ubuntu@ec2-18-194-112-217.eu-central-1.compute.amazonaws.com "cd pyStonks && git pull origin main && pipenv install --deploy --ignore-pipfile"
+ssh -i "pystonks.pem" ubuntu@ec2-18-194-112-217.eu-central-1.compute.amazonaws.com "cd pyStonks/server && git pull origin main && pipenv install --deploy --ignore-pipfile"
 
 # git add .
 # git commit -m "Deploying"

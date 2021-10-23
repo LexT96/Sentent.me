d29e048f46580ed8a0b03399c2fb7ed1f5d61fc2
Thorsten <tlex1@web.de>
2021-10-23 11:42:39 +0200

    Add necessary headers to api request

--- server/app/services/stock_api_service.py
+++ server/app/services/stock_api_service.py
@@ -3,8 +3,19 @@ import requests
 def fetch_tickers():
     url = 'https://api.nasdaq.com/api/screener/stocks?download=true'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10.15 rv: 90.0) Gecko/20100101 Firefox/90.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+        'User-Agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10.15 rv: 90.0) Gecko/20100101 Firefox/93.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'Accept-Language: de,en-US;q=0.7,en;q=0.3',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': "none",
+        'Sec-Fetch-User': '?1',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'TE': 'trailer',
+        }
     number_of_tries = 0
     while number_of_tries < 4: 
         request = requests.get(url, headers=headers)
@@ -13,6 +24,5 @@ def fetch_tickers():
         if stocks:
             return stocks
         number_of_tries += 1
-    return None    
-
+    return None 
 

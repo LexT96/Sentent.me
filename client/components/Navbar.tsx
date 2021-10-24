29bd0c4d84f3d0812ee260a989fa5da1e2dd86ac
Thorsten <tlex1@web.de>
2021-10-24 16:36:07 +0200

    Handle scroll on navbar brand click

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -6,20 +6,24 @@ import {
   FormControl,
   Navbar as BSNavbar,
 } from "react-bootstrap";
+import Searchbar from "./Searchbar";
 
 const Navbar = () => {
   const [isTransparent, setIsTransparent] = useState(true);
   const handleScroll = (e: Event) => {
-    if (
-      window.pageYOffset > (window.innerHeight * 0.6)
-    ) {
+    if (window.pageYOffset > window.innerHeight * 0.6) {
       setIsTransparent(false);
       return;
     }
-    if (window.pageYOffset < (window.innerHeight * 0.6)) {
+    if (window.pageYOffset < window.innerHeight * 0.6) {
       setIsTransparent(true);
     }
   };
+
+  const onBrandingClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }  
+  
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -31,12 +35,18 @@ const Navbar = () => {
         id="navbar"
       >
         <Container>
-          <BSNavbar.Brand href="#home">
+          <BSNavbar.Brand onClick={onBrandingClick} style={{cursor: "pointer"}}>
             <strong style={{ color: "white" }}>Sentent.me</strong>
           </BSNavbar.Brand>
-          <a href="https://github.com/LexT96/pyStonks">
-            <img height={32} width={32} src="/assets/github.png" alt="Github" />
-          </a>
+              <a href="https://github.com/LexT96/pyStonks">
+                <img
+                  height={32}
+                  width={32}
+                  src="/assets/github.png"
+                  className="icon"
+                  alt="Github"
+                />
+              </a>
         </Container>
       </BSNavbar>
     </>
